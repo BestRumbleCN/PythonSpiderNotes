a30949955d8b2d739a339cc71f4197e1117b1057
1204165659@qq.com <1204165659@qq.com>
2017-12-01 10:09:41 +0800

    完善第一个爬虫

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PythonSpiderNotes
 python爬虫笔记
+
+主要参考：https://github.com/lining0806/PythonSpiderNotes
+
--- douban_book/collect20books.py
+++ douban_book/collect20books.py
@@ -1,21 +1,59 @@
 '''
-Created on 2017年11月30日
+收集豆瓣图书python分类下前20本书
 
-@author: fly
+@author: xfx
 '''
 from bs4 import BeautifulSoup
 from urllib import request
-content = request.urlopen("https://book.douban.com/tag/python?start=0&type=T")
-soup = BeautifulSoup(content)
-#print(soup.head)
-items = soup.find_all('li', "subject-item")
-for item in items:
-    img = item.find('a').img.get("src")
-    infoNode = item.find('div',"info")
-    title = infoNode.h2.a.get("title")
-    baseinfo = infoNode.find('div',"pub").string
-    starinfo = infoNode.find('div',"star").find_all("span")
-    star = starinfo[1].string
-    amount = starinfo[2].string
-    print("title:{},image:{},baseInfo:{}".format(title,img,baseinfo))
-    print("star:{},amount:{}".format(star,amount))
+import os
+
+def writeToFile(save_path,filename,content):
+    '''  
+    将文本内容追加到指定目录文件中，若目录或文件不存在则先创建
+    :Args:
+     - save_path - 文件所在目录
+     - filename - 文件名（带后缀）
+     - content - 追加的文本内容
+    '''
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    path = save_path+"/"+filename
+    with open(path,"a+") as fp:
+        fp.write(content)
+
+def readSource():
+    content = request.urlopen("https://book.douban.com/tag/python?start=0&type=T")
+    soup = BeautifulSoup(content)
+    items = soup.find_all('li', "subject-item")
+    for item in items:
+        img = item.find('a').img.get("src")
+        infoNode = item.find('div',"info")
+        title = infoNode.h2.a.get("title")
+        baseinfo = infoNode.find('div',"pub").string
+        starinfo = infoNode.find('div',"star").find_all("span")
+        star = starinfo[1].string
+        amount = starinfo[2].string
+        book_item = bookItem(title,baseinfo,star,amount,img)
+        writeToFile("D:\\", "豆瓣图书.txt", book_item.itemString())
+
+class bookItem:
+    '''
+    图书对象
+    '''
+    def __init__(self,title,base_info,star,star_amount,img):
+        self.title=title.strip()
+        self.base_info = base_info.strip()
+        self.star = star.strip()
+        self.star_amount = star_amount.strip()
+        self.img = img.strip()
+
+    def itemString(self):
+        result = "-------------------------------------------------------\n"
+        result += self.title+"\n"
+        result += self.base_info+"\n"
+        result += self.star+"\n"
+        result += self.star_amount+"\n"
+        return result
+
+if __name__ == '__main__':
+    readSource()
